843625955c56f5cf1b0285fa55e4edcce4aaddca
Keith Frazier <keithers98@gmail.com>
2023-01-12 15:56:11 -0800

    added types for native data storage

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,22 @@ export type GoalTypes = "Monthly" | "Daily" | "Yearly";
 export interface GoalsState {
   goalsToEdit: GoalTypes | null;
 }
+export interface Goal {
+  type: GoalTypes;
+  id: string;
+  title: string;
+  PointValue: number;
+  isActive: boolean;
+  created: number;
+  inactivated: number;
+}
+
+export interface GoalProgress {
+  [goalId: string]: boolean;
+}
+
+export interface EntryMetadata {
+  id: string;
+  created: string;
+  snipped: string;
+}
